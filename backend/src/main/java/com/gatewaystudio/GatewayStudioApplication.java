2662129343b0b01d341d0effdc86355f10b0392a
Sumanth Hegde <sumanthhegde13@gmail.com>
2026-08-30 13:13:58 +0530

    initialization

---
+++ backend/src/main/java/com/gatewaystudio/GatewayStudioApplication.java
@@ -0,0 +1,13 @@
+package com.gatewaystudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayStudioApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/gatewaystudio/GatewayStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gatewaystudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
